aba43acac7a083043b69f9d74a28d0cf4a978b42
MakarSolomatin <topdoggyp@gmail.com>
2016-06-09 18:06:35 +0300

    1

--- js/capture/colortracking.js
+++ js/capture/colortracking.js
@@ -1,19 +1,32 @@
-function TrackColor()
+function screentracker ()
 {
-  var colors = new tracking.ColorTracker(["magenta"]);
+  var self = this;
 
-  colors.on('track', function (e)
+  self.trackX = 0;
+  self.trackY = 0;
+  self.trackW = 0;
+  self.trackH = 0;
+     
+  self.TrackColor = function()
   {
-    if (e.data.length === 0)
-      console.log('Cannot find any color')
-    else
-    {
-      e.data.forEach(function (rect)
-      {
-        console.log(rect.x, rect.y, rect.width, rect.height, rect.color);
-      })
-    }
-  });
+    var colors = new tracking.ColorTracker(["magenta"]);
 
-  tracking.track("#video", colors);
+    colors.on('track', function (e)
+    {
+      if (e.data.length === 0)
+        console.log('Cannot find any color');
+      else
+      {
+        e.data.forEach(function (rect)
+        {
+          self.trackX = rect.x;
+          self.trackY = rect.y;
+          self.trackW = rect.width;
+          self.trackH = rect.height;
+        })
+      }
+    });
+
+    tracking.track("#video", colors);
+  }
 }
--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ function StartRender()
   var A = new anim();
 
   A.MainCamera.position.z = 5;
-  A.AddMesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial( { color: 0xff0000 } ), function(){});
+  A.AddMesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial( { color: 0xff0000 } ), function(mesh){mesh.rotation.set(A.time, A.time, 0)});
 
   A.Start();
-
 }
